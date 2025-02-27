f08e0e39628ae385aecf5c947871ddd6e9d422ef
Ryan Ng <121779670+ryanng2008@users.noreply.github.com>
2025-02-27 21:43:41 +0800

    results

--- src/app/home/browse/[listing]/loading.tsx
+++ src/app/home/browse/[listing]/loading.tsx
@@ -17,21 +17,21 @@ export default function Loading() {
                     <h1 className="font-semibold text-2xl">Details</h1>
                     <div className="flex gap-4 items-center">
                         <MapPinIcon className="h-8" />
-                        <p>Tijuana, Mexico</p>
+                        <p>...</p>
                     </div>
                     <div className="flex gap-4 items-center">
                         <CurrencyDollarIcon className="h-8" />
-                        <p>$50-$10000</p>
+                        <p>...</p>
                     </div>
                 </div>
                 <div className="space-y-4">
                     <h1 className="font-semibold text-2xl">About Leonardo</h1>
                     <ul className="list-disc list-inside text-sm">
-                        <li>Pulled up in a Beamer I was flexing</li>
-                        <li>Poured a cup of Wok cuz I was stressing</li>
-                        <li>Put on all my ice now I feel so cool</li>
+                        <li>...</li>
+                        <li>...</li>
+                        <li>...</li>
                     </ul>
-                    <p className="text-sm">Von-ipsum dolor sit da block, you know how it go. Streets be wild, opps be mad, and shorties steady tweakin. Aint no cappin, we out here stackin, catchin bands and chasin dreams. Real ones gon relate, fake ones gon hate, but thats just how the game play out. Hundred nights, hundred fights, still I rise up from the trenches. Keep it hunnid, keep it real, thats the only way I feel. You hear me? Yeah, you hear me. Thats on everything.</p>
+                    <p className="text-sm">...</p>
                 </div>
             </div>
             <div className="flex flex-col ">
